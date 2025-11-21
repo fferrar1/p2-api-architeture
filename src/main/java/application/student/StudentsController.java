9d5f61ede0c3aca3dadae0c0b9ff5d9af36ed33b
Fernanda Ferreira Ferrari <110040514+fferrar1@users.noreply.github.com>
2025-11-21 20:05:12 +0000

    [FIX] Correção no controller do aluno

--- src/main/java/application/student/StudentsController.java
+++ src/main/java/application/student/StudentsController.java
@@ -3,8 +3,10 @@ package application.student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,7 +17,7 @@ public class StudentsController {
     private StudentsService studentsService;
 
     @PostMapping
-    public StudentsDTO insert(StudentsInsertDTO student) {
+    public StudentsDTO insert(@RequestBody StudentsInsertDTO student) {
         return studentsService.insert(student);
     }
 
@@ -25,17 +27,17 @@ public class StudentsController {
     }
 
     @GetMapping("/{id}")
-    public StudentsDTO getOne(long id) {
+    public StudentsDTO getOne(@PathVariable long id) {
         return studentsService.getOne(id);
     }
 
     @PutMapping("/{id}")
-    public StudentsDTO update(long id, StudentsInsertDTO student) {
+    public StudentsDTO update(@PathVariable long id, @RequestBody StudentsInsertDTO student) {
         return studentsService.update(id, student);
     }
 
     @DeleteMapping("/{id}")
-    public void delete(long id) {
+    public void delete(@PathVariable long id) {
         studentsService.delete(id);
     }
 }
