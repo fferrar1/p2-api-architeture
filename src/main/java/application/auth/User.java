4e2163f88ebe2b75f7f9c2f6c5af8d7ee6d1dfd4
Fernanda Ferreira Ferrari <110040514+fferrar1@users.noreply.github.com>
2025-11-21 21:17:08 +0000

    [NEW] Criando o usuário e o repositório

---
+++ src/main/java/application/auth/User.java
@@ -0,0 +1,26 @@
+package application.auth;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(name = "username", nullable = false, unique = true)
+    private String username;
+    @Column(name = "password", nullable = false)
+    private String password;
+}
